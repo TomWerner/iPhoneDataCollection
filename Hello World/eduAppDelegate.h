25bdfefb911126c7242ffb631597b89e1891a157
Tom Werner <tomwerner@dhcp80ffe120.residence-rooms.uiowa.edu>
2014-05-02 19:11:24 -0500

    Initial Commit

---
+++ Hello World/eduAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  eduAppDelegate.h
+//  Hello World
+//
+//  Created by Tom Werner on 5/2/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface eduAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hello World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Tom Werner on 5/2/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "eduAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([eduAppDelegate class]));
+    }
+}
